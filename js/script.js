e7fb5aec2e2ed3d3542be728074298ae64bf4a31
Nadia Gushin <nadiagushin@Nadias-MacBook-Pro.local>
2024-05-07 22:03:24 -0400

    Slot symbol image sources accessible in JavaScript array && HTML accepts image sources from JavaScript

--- js/script.js
+++ js/script.js
@@ -7,48 +7,76 @@ const slotSymbols = [
     {
         symbolPosition: 1,
         symbolName: "gold-dragon",
+        imageSource: `<img src="../images/dragon-gold.png">`
     },
     {
         symbolPosition: 2,
-        symbolName: "wild",
+        symbolName: "white-egg",
+        imageSource: `<img src="../images/egg-white.png">`
     },
     {
         symbolPosition: 3,
         symbolName: "color-dragon",
+        imageSource: `<img src="../images/dragon-multi.png">`
     },
     {
         symbolPosition: 4,
         symbolName: "seven",
+        imageSource: `<img src="../images/seven.png">`
     },
     {
         symbolPosition: 5,
         symbolName: "white-dragon",
+        imageSource: `<img src="../images/dragon-white.png">`
     },
     {
         symbolPosition: 6,
         symbolName: "jackpot",
+        imageSource: `<img src="../images/jackpot.png">`
     },
     {
         symbolPosition: 7,
         symbolName: "gold-egg",
+        imageSource: `<img src="../images/egg-gold.png">`
     },
     {
         symbolPosition: 8,
         symbolName: "fire-bowl",
+        imageSource: `<img src="../images/fire-bowl.png">`
     },
     {
         symbolPosition: 9,
         symbolName: "color-egg",
+        imageSource: `<img src="../images/egg-multi.png">`
     },
     {
         symbolPosition: 10,
-        symbolName: "white-egg",
+        symbolName: "wild",
+        imageSource: `<img src="../images/wild.png">`
     },
 ]
 
-const reelOne = document.querySelector("#reel-one");
-const reelTwo = document.querySelector("#reel-two");
-const reelThree = document.querySelector("#reel-three");
+let oneTop = document.getElementById("onetoptarget");
+onetoptarget.innerHTML = slotSymbols[0].imageSource;
+let oneMiddle = document.getElementById("onemiddletarget");
+onemiddletarget.innerHTML = slotSymbols[1].imageSource;
+let oneBottom = document.getElementById("onebottomtarget");
+onebottomtarget.innerHTML = slotSymbols[2].imageSource;
+
+
+let twoTop = document.getElementById("twotoptarget");
+twotoptarget.innerHTML = slotSymbols[3].imageSource;
+let twoMiddle = document.getElementById("twomiddletarget");
+twomiddletarget.innerHTML = slotSymbols[4].imageSource;
+let twoBottom = document.getElementById("twobottomtarget");
+twobottomtarget.innerHTML = slotSymbols[5].imageSource;
+
+let threeTop = document.getElementById("threetoptarget");
+threetoptarget.innerHTML = slotSymbols[6].imageSource;
+let threeMiddle = document.getElementById("threemiddletarget");
+threemiddletarget.innerHTML = slotSymbols[7].imageSource;
+let threeBottom = document.getElementById("threebottomtarget");
+threebottomtarget.innerHTML = slotSymbols[8].imageSource;
 
 let credits = 500;
 let currentWager = 0;
@@ -90,6 +118,7 @@ function betThree() {
 // "Spin" Reels: Only 3 positions on the reel visible, Animate change
 // Execute a random number generator
 function spin(reel) {
+    console.log("I should spin.")
     Math.round(Math.random * slotSymbols.length);
 }
 
@@ -98,6 +127,7 @@ function spin(reel) {
 // Return outcome
 // Play button either disabled until wager, or makes default wager of one credit
 function play() {
+    console.log("Game should start.")
     spin(reelOne);
     spin(reelTwo);
     spin(reelThree);
@@ -105,7 +135,8 @@ function play() {
 
 // Need a function to "evaluate" results along the play line to determine outcome.
 function outcome() {
-    return winnings
+    console.log("I update credits according to outcome.")
+    return winnings;
 }
 
 //Need a function to regulary update values along the credit line
