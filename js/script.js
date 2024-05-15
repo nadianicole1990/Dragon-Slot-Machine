24818e71456d795b7510d2054e5c646ab0459408
Nadia Gushin <nadiagushin@Nadias-MacBook-Pro.local>
2024-05-15 16:45:10 -0400

    Establish reel reset

--- js/script.js
+++ js/script.js
@@ -115,12 +115,18 @@ const reelThreeOutcome = [
     }
 ]
 
-let a = Math.round(Math.random() * (reelOneOutcome.length-1));
-console.log(a);
-let b = Math.round(Math.random() * (reelTwoOutcome.length-1));
-console.log(b);
-let c = Math.round(Math.random() * (reelThreeOutcome.length-1));
-console.log(c);
+let a;
+let b;
+let c;
+
+function generateRandoms() {
+    a = Math.round(Math.random() * (reelOneOutcome.length-1));
+    console.log(a);
+    b = Math.round(Math.random() * (reelTwoOutcome.length-1));
+    console.log(b);
+    c = Math.round(Math.random() * (reelThreeOutcome.length-1));
+    console.log(c);
+}
 
 function spinOne() {
     document.getElementById("reel-one").style.animation = reelOneOutcome[a].animation;
@@ -139,47 +145,36 @@ let win;
 function comparePayline() {
     if (reelOneOutcome[a].symbol === "gold-dragon" && reelTwoOutcome[b].symbol === "gold-dragon" && reelThreeOutcome[c].symbol === "gold-dragon") {
         win = true;
-        winnings = 5;
         credits = credits + 5;
         } else { if (reelOneOutcome[a].symbol === "white-egg" && reelTwoOutcome[b].symbol === "white-egg" && reelThreeOutcome[c].symbol === "white-egg") {
             win = true;
-            winnings = 125;
             credits = credits + 125;
             } else { if (reelOneOutcome[a].symbol === "color-dragon" && reelTwoOutcome[b].symbol === "color-dragon" && reelThreeOutcome[c].symbol === "color-dragon") {
                 win = true;
-                winnings = 25;
                 credits = credits + 25;
                 } else { if (reelOneOutcome[a].symbol === "seven" && reelTwoOutcome[b].symbol === "seven" && reelThreeOutcome[c].symbol === "seven") {
                     win = true;
-                    winnings = 75;
                     credits = credits + 75;
                     } else { if (reelOneOutcome[a].symbol === "white-dragon" && reelTwoOutcome[b].symbol === "white-dragon" && reelThreeOutcome[c].symbol === "white-dragon") {
                         win = true;
-                        winnings = 1250;
                         credits = credits + 1250;
                         } else { if (reelOneOutcome[a].symbol === "jackpot" && reelTwoOutcome[b].symbol === "jackpot" && reelThreeOutcome[c].symbol === "jackpot") {
                             win = true;
-                            winnings = 15;
                             credits = credits + 15;
                             } else { if (reelOneOutcome[a].symbol === "gold-egg" && reelTwoOutcome[b].symbol === "gold-egg" && reelThreeOutcome[c].symbol === "gold-egg") {
                                 win = true;
-                                winnings = 3;
                                 credits = credits + 3;
                                 } else { if (reelOneOutcome[a].symbol === "fire-bowl" && reelTwoOutcome[b].symbol === "fire-bowl" && reelThreeOutcome[c].symbol === "fire-bowl") {
                                 win = true;
-                                winnings = 10;
                                 credits = credits + 10;
                                     } else { if (reelOneOutcome[a].symbol === "color-egg" && reelTwoOutcome[b].symbol === "color-egg" && reelThreeOutcome[c].symbol === "color-egg") {
                                     win = true;
-                                    winnings = 1;
                                     credits = credits + 1;
                                         } else { if (reelOneOutcome[a].symbol === "wild" && reelTwoOutcome[b].symbol === "wild" && reelThreeOutcome[c].symbol === "wild") {
                                         win = true;
-                                        winnings = 250;
                                         credits = credits + 250;
                                             } else { if (reelOneOutcome[a].symbol === "color-egg" && reelTwoOutcome[b].symbol === "color-egg" && reelThreeOutcome[c].symbol === "color-egg") {
                                             win = true;
-                                            winnings = 1;
                                             credits = credits + 1;
                                                 } else {
                                                 win = false;
@@ -210,6 +205,7 @@ function outcome() {
 
 function play() {
     credits = credits-1
+    generateRandoms();
     spinOne();
     spinTwo();
     spinThree();
@@ -221,9 +217,9 @@ const playButton = document.querySelector("#trigger-round");
 playButton.addEventListener("click", play);
 
 function reset() {
-    document.getElementById("reel-one").style.animation = none;
-    document.getElementById("reel-one").style.animation = none;
-    document.getElementById("reel-one").style.animation = none;
+    document.getElementById("reel-one").style.animation = "reset";
+    document.getElementById("reel-two").style.animation = "reset";
+    document.getElementById("reel-three").style.animation = "reset";
     winnings = 0;
 }
 
@@ -240,8 +236,6 @@ document.getElementById("exitlose").addEventListener("click", function() {
 setInterval(function() {
     const creditsDisplay = document.getElementById("creditstarget");
     creditstarget.innerHTML = credits;
-    const currentWinDisplay = document.getElementById("winningstarget");
-    winningstarget.innerHTML = winnings;
 }, 500)
 
 document.getElementById("open-paytable").addEventListener("click", function() {
